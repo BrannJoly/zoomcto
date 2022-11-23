dd3c375b341318754927111456f978143d3bc59f
Brann Joly <joly.brann@gmail.com>
2022-11-23 21:17:02 +0100

    added a datepicker and a timepicker to the popup

--- client/src/ZoomCallCreationComponent.tsx
+++ client/src/ZoomCallCreationComponent.tsx
@@ -1,5 +1,6 @@
 import { Fragment, useState } from "react";
-
+import DatePicker from "react-date-picker";
+import TimePicker from "react-time-picker";
 
 interface ZoomCallCreationComponentProps {
     onEventCreated: (start: Date, end: Date, topic: string) => void
@@ -8,23 +9,35 @@ interface ZoomCallCreationComponentProps {
     closeFunction: () => void;
 }
 
+var getDate = (date: Date, time: string) => {
+    var d = new Date(date);
+    var [h, m] = time.split(':')
+    d.setHours(parseInt(h));
+    d.setMinutes(parseInt(m));
+    return d;
+}
 
 export default function ZoomCallCreationComponent({ onEventCreated, initialStart, initialEnd, closeFunction }: ZoomCallCreationComponentProps) {
 
-    const [startDate, setstartDate] = useState(initialStart);
-    const [endDate, setendDate] = useState(initialEnd);
+    const [eventDate, seteventDate] = useState(initialStart);
+
+    const [startTime, setstartTime] = useState(`${initialStart.getHours()}:${initialStart.getMinutes()}`);
+    const [endTime, setendTime] = useState(`${initialEnd.getHours()}:${initialEnd.getMinutes()}`);
     const [topic, setTopic] = useState("Objet");
 
     return (<Fragment>
-
-        <form >
-            <label>
-                Objet:
-                <textarea value={topic} onChange={e => setTopic(e.target.value)} />
-            </label>
-
-        </form>
-
+        <div className="content" >
+            <DatePicker onChange={seteventDate} value={eventDate} />
+        </div>
+        <div className="content" >
+            <TimePicker disableClock={true} onChange={(e: any) => setstartTime(e)} value={startTime} />
+        </div>
+        <div className="content" >
+            <TimePicker disableClock={true} onChange={(e: any) => setendTime(e)} value={endTime} />
+        </div>
+        <div className="content" >
+            <textarea value={topic} onChange={e => setTopic(e.target.value)} />
+        </div>
 
         <button className="button" onClick={() => { console.log('modal closed '); closeFunction(); }}>
             Annuler
@@ -32,7 +45,7 @@ export default function ZoomCallCreationComponent({ onEventCreated, initialStart
 
         <button className="button"
             onClick={() => {
-                onEventCreated(startDate, endDate, topic);
+                onEventCreated(getDate(eventDate, startTime), getDate(eventDate, endTime), topic);
             }}
         >
             Valider
